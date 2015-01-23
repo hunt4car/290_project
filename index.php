efe709b02abfb848abc1b127bfa93c1fdfc28f2b
Hunter J Nelson <hunt4car@gmail.com>
2015-01-22 16:30:31 -0800

    Start
    added folder structure index and style.css

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>290_project</title>
+	<link rel="stylesheet" href="includes/css/style.css">
+</head>
+<body>
+	
+</body>
+</html>
