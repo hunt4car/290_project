b8538268108d86d008c4c25849e38b899cdc69df
Hunter J Nelson <hunt4car@gmail.com>
2015-03-08 20:13:11 -0700

    mid api work
    documentation of much api experimentation

---
+++ api_read.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>test</title>
+	<script type='text/javascript' src='http://code.jquery.com/jquery-1.11.0.min.js'></script>
+	<script type='text/javascript' src='includes/js/json2html.js'></script>
+	<script type='text/javascript' src='includes/js/jquery.json2html.js'></script>
+	<script type='text/javascript' src='includes/js/helper.js'></script>
+	<link rel="StyleSheet" href="includes/css/helper.css" type="text/css"/>
+	<!-- <script type='text/javascript' src=''></script> -->
+</head>
+
+<!-- <script type='text/javascript' src='http://code.jquery.com/jquery-1.11.0.min.js'></script> -->
+<script type="text/javascript">
+// var html = "http://localhost:8888/290_project/api_write.php";	
+
+// document.write(html);
+
+var url;
+$.ajax({
+    url : "http://localhost:8888/290_project/api_write.php",
+    success : function(result){
+    	var url = result;
+        document.write(url);
+        var html = json2html(url);
+    	document.write(html);
+
+    }
+   
+});
+
+// document.write(url);
+// var html = json2html(url);
+//     document.write(html);
+
+
+</script>
+<body>	
+</body>
+</html>
+
---
+++ api_write.php
@@ -0,0 +1,27 @@
+<?php 
+$pagetitle = "index";
+include 'includes/secure_includes.php';
+// include 'includes/post_section.php';
+
+$user_id = 15;  //"url content"
+
+$sql = "SELECT * FROM posts
+            WHERE `user_id` = '$user_id' AND post_date > CURDATE() - INTERVAL 2 YEAR
+            ORDER BY post_date DESC ";
+            $result = $dbc->query($sql);
+
+            
+            //Create an array
+    $json_response = array();
+    
+    while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {
+        $row_array['post_date'] = $row['post_date'];
+        $row_array['post_content'] = $row['post_content'];
+        
+        //push the values in the array
+        array_push($json_response,$row_array);
+    }
+    echo json_encode($json_response);
+
+
+             ?>
